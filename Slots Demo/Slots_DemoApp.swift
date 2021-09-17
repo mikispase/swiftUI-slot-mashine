427b0af046f83e1738d97a3e7dde1cbedfcc16b2
Dimitar Spasovski <dimitar.spasovski@movic.nl>
2021-09-17 05:42:54 +0200

    Initial Commit

---
+++ Slots Demo/Slots_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_DemoApp.swift
+//  Slots Demo
+//
+//  Created by Dimitar Spasovski on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Slots_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
